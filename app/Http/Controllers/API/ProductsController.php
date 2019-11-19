581b48780e28d0e5581aac69b9aa73dd0247841a
Mohamed Riyas <mail@riyas.co.in>
2019-11-20 00:35:47 +0800

    Product API updated with offers

--- app/Http/Controllers/API/ProductsController.php
+++ app/Http/Controllers/API/ProductsController.php
@@ -18,10 +18,16 @@ class ProductsController extends Controller
 			$text = null;
 			@$discount_details = Discount::where('product_id', $p->id)->where('role_id', $role_id)->get();
 			if(@$discount_details[0]->method == 'Flat'){
-				$text = 'Buy '.$discount_details[0]->min_qty.' or more RM '.$discount_details[0]->flat_price.' per unit';
+				if($discount_details[0]->min_qty == 1){
+					$text = 'Price dropped to RM '.$discount_details[0]->flat_price.' per unit';;
+				}
+				else{
+					$text = 'Buy '.$discount_details[0]->min_qty.' or more RM '.$discount_details[0]->flat_price.' per unit';
+				}
 			}
 			elseif(@$discount_details[0]->method == 'Extra'){
-				$text = 'Get a '.$discount_details[0]->free_qty.' for '.$discount_details[0]->min_qty.' deal';
+				$total_qty = $discount_details[0]->free_qty + $discount_details[0]->min_qty;
+				$text = 'Get '.$total_qty.' for '.$discount_details[0]->min_qty.' deal';
 			}
 
 			$product = [
